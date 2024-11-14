6f99c4035bcdc4e8ec595eb84ad4f395fe348766
Polyshinnel <polyshinnel@gmail.com>
2024-11-14 16:50:11 +0300

    add other service page

--- resources/views/commonPages/Services/Common.blade.php
+++ resources/views/commonPages/Services/Common.blade.php
@@ -1,5 +1,174 @@
 @extends('layouts.base_layout')
-@section('page_title', 'Общестроительные')
+@section('page_title', 'Электрика')
 
 @section('content')
+    <div class="box-container">
+        <ul class="breadcrumbs">
+            <li><a href="/">Главная</a>&nbsp;<span>-</span>&nbsp;</li>
+            <li><a href="/services">Услуги</a>&nbsp;<span>-</span>&nbsp;</li>
+            <li>Общестрой</li>
+        </ul>
+        <!-- /.breadcrumbs -->
+
+        <h1 class="page-title">Комплексное снабжение — Общестрой</h1>
+
+        <div class="services-links">
+            <a href="/services/electrician">
+                <div class="services-link">
+                    <img src="/assets/img/icons/services/electric.svg" alt="">
+                    <p>Электрика</p>
+                </div>
+            </a>
+
+            <a href="/services/plumber">
+                <div class="services-link">
+                    <img src="/assets/img/icons/services/plumber.svg" alt="">
+                    <p>Сантехника</p>
+                </div>
+            </a>
+
+            <a href="/services/ventilation">
+                <div class="services-link">
+                    <img src="/assets/img/icons/services/ventilation.svg" alt="">
+                    <p>Вентиляция</p>
+                </div>
+            </a>
+
+            <a href="/services/common">
+                <div class="services-link services-link_active">
+                    <img src="/assets/img/icons/services/common.svg" alt="">
+                    <p>Общестрой</p>
+                </div>
+            </a>
+        </div>
+        <!-- /.services-links -->
+
+
+        <div class="service-container">
+            <div class="service-container__content">
+                <div class="service-container__content-columns">
+                    <div class="service-container__content-column service-container__content-column_big">
+                        <h2>Строительные материалы</h2>
+
+                        <ul>
+                            <li>Сухие смеси, теплоизоляция, древесно-плитные материалы,пеноблок, кирпич</li>
+                            <li>Метизы, кровля, сайдинг, водосточные системы</li>
+                            <li>Лакокрасочные материалы, ручной инструмент, хозтовары, пены, клей, герметики</li>
+                            <li>Напольные покрытия, садовые и сезонные товары</li>
+                            <li>Двери входные и межкомнатные</li>
+                            <li>Электроинструмент и комплектующие</li>
+                            <li>Санфаянс, мебель для ванных комнат, душевые кабины и ванны</li>
+                            <li>Керамическая плитка, исскуственный камень, керамогранит</li>
+                            <li>Щебень, песок, бетон</li>
+                        </ul>
+                    </div>
+                    <!-- /.service-container__content-column -->
+
+                    <div class="service-container__content-column service-container__content-column_sm">
+                        <h2>Сроки поставки</h2>
+
+                        <p>от 2 до 3 дней</p>
+
+                        <div class="service-container__content-column-links">
+                            <a href="/">
+                                <div class="service-container__content-column-link">
+                                    <img src="/assets/img/icons/documents.svg" alt="">
+                                    <p>Бланк договора</p>
+                                </div>
+                            </a>
+
+                            <a href="/delivery">
+                                <div class="service-container__content-column-link">
+                                    <img src="/assets/img/icons/delivery.svg" alt="">
+                                    <p>Условия доставки</p>
+                                </div>
+                            </a>
+                        </div>
+                        <!-- /.service-container__content-column-links -->
+                    </div>
+                    <!-- /.service-container__content-column -->
+                </div>
+                <!-- /.service-container__content-columns -->
+
+                <div class="service-container__content-stages-block">
+                    <h2>Этапы начала сотрудничества</h2>
+
+                    <div class="service-container__content-stages">
+                        <div class="service-container__content-stage">
+                            <h3>01</h3>
+                            <p>Вы оставляете заявку, через любую форму на сайте. При необходимости прикрепите документы или фото.</p>
+                        </div>
+                        <!-- /.service-container__content-stage -->
+
+                        <div class="service-container__content-stage">
+                            <h3>02</h3>
+                            <p>Менеджер связывается с Вами. Уточняет детали. Отправляет Вам коммерческое предложение.</p>
+                        </div>
+                        <!-- /.service-container__content-stage -->
+
+                        <div class="service-container__content-stage">
+                            <h3>03</h3>
+                            <p>Заключаем договор и поставляем заказанный Вами товар</p>
+                        </div>
+                        <!-- /.service-container__content-stage -->
+                    </div>
+                    <!-- /.service-container__content-stages -->
+                </div>
+                <!-- /.service-container__content-stages-block -->
+
+                <div class="advantages-block">
+                    <h2>В чем выгода?</h2>
+
+                    <p>Комплексное снабжение имеет целый ряд преимуществ, в силу которых его выбирают строительные компании, которые серьезно относятся к планированию своего времени и расходов:</p>
+
+                    <ul>
+                        <li>Качество материалов и соответствие их ГОСТам или ТУ контролируется централизованно.</li>
+                        <li>Комплектация производится без вмешательства заказчика в организационный процесс снабжения.</li>
+                        <li>Выбираются лучшие оптовые цены.</li>
+                        <li>Снижается риск недобросовестной конкуренции между поставщиками.</li>
+                        <li>Снижается количество контрагентов заказчика, упрощается ведение документации.</li>
+                    </ul>
+                </div>
+                <!-- /.advantages-block -->
+            </div>
+            <!-- /.service-container__content -->
+
+            <div class="service-container__form-block">
+                <form class="service-container__form">
+                    <h2>Заказ оборудования</h2>
+
+                    <div class="input-block">
+                        <input type="text" name="name" id="name" placeholder="Ваше имя *">
+                        <span class="err-text">Имя должно быть не менее 2х символов!</span>
+                    </div>
+                    <!-- /.input-block -->
+
+                    <div class="input-block">
+                        <input type="text" name="email" id="email" placeholder="Email *">
+                        <span class="err-text">Введите корректную почту вида example@example.com!</span>
+                    </div>
+                    <!-- /.input-block -->
+
+                    <div class="input-block">
+                        <input type="text" name="phone" id="phone" placeholder="Телефон *">
+                        <span class="err-text">Введите корректный телефон вида +7(999)999-99-99</span>
+                    </div>
+                    <!-- /.input-block -->
+
+                    <div class="input-block">
+                        <div class="input-block__checkbox">
+                            <input type="checkbox" name="instalation" id="instalation">
+                            <label for="instalation">Необходимо провести монтаж</label>
+                        </div>
+                    </div>
+                    <!-- /.input-block -->
+
+                    <input type="submit" value="Отправить заявку" class="base-btn send-demand">
+                </form>
+                <!-- /.service-container__form -->
+            </div>
+            <!-- /.service-container__form-block -->
+        </div>
+        <!-- /.service-container -->
+    </div>
 @endsection
